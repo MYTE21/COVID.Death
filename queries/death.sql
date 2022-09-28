8a540e3c424f0ea5dfb931262f09f5b8aa1dc83b
Md. Yasmi Tohabar Evon (2016831021) <silverevon10010@gmail.com>
2022-09-28 19:32:31 +0600

    Total cases vs total deaths

--- queries/death.sql
+++ queries/death.sql
@@ -9,8 +9,17 @@ FROM covid_death
 ORDER BY 3, 4;
 
 /* Select `location`, `date`, `total_cases`, `new_cases`, `total_deaths` and `population`
-   from the table `covid_death`
+   from the table `covid_death`.
+   Sorted by `location` and `date`.
  */
 SELECT location, date, total_cases, new_cases, total_deaths, population
 FROM covid_death
+ORDER BY 1, 2;
+
+/* Total Cases VS Total Deaths
+   - Show likelihood of dying if you contract Covid-19 in United States.
+ */
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases) * 100 AS death_percentage
+FROM covid_death
+WHERE location LIKE '%states%'
 ORDER BY 1, 2;
